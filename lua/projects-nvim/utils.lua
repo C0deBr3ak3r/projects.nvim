a4fe8f777c342b01b9fae3b4a99634f06bb89e65
C0deBr3ak3r <c0debr3ak3r.git@gmail.com>
2024-06-23 20:44:34 -0300

    refactor(utils): change some structures

--- lua/projects-nvim/utils.lua
+++ lua/projects-nvim/utils.lua
@@ -1,16 +1,5 @@
 local M = {}
 
----@param msg string
----@param log_level? integer One of vim.log.levels
----@param opts? table
-function M.notify(msg, log_level, opts)
-	vim.schedule(function()
-		vim.notify(msg, log_level, opts)
-	end)
-end
-
-vim.g.projects_nvim_config = vim.g.projects_nvim_config or {}
-
 local levels = {
 	[0] = 'TRACE',
 	[1] = 'DEBUG',
@@ -20,23 +9,29 @@ local levels = {
 	[5] = 'OFF',
 }
 
-local minimun_level = vim.g.projects_nvim_config.log_level or vim.log.levels.WARN
-
 ---@type file*
 local log_file = nil
 
-local function open_log_file()
-	local path = vim.g.projects_nvim_config.log_file or vim.fn.stdpath('log') .. '/projects.log'
+---@param msg string
+---@param log_level? integer One of vim.log.levels
+---@param opts? table
+function M.notify(msg, log_level, opts)
+	vim.schedule(function()
+		vim.notify(msg, log_level, opts)
+	end)
+end
+
+function M.set_log_file(path)
+	if log_file then
+		M.notify("Cannot set a new log file after it's already open", vim.log.levels.ERROR)
+		return
+	end
 
 	local file, err = io.open(path, 'a+')
 
 	if not file then
 		M.notify(string.format('Cannot open project.nvim log file: %s', err))
 		return
-	end
-
-	if log_file then
-		log_file:close()
 	end
 
 	log_file = file
@@ -46,12 +41,8 @@ end
 ---@param level integer? One of the values of vim.log.levels
 function M.log(msg, level)
 	level = level or vim.log.levels.DEBUG
-	if level < minimun_level then
+	if level < (vim.g.projects_nvim_config.log_level or vim.log.levels.WARN) then
 		return
-	end
-
-	if not log_file then
-		open_log_file()
 	end
 
 	log_file:write(
